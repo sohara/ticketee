0fe0d08b057efdd2b10bdbbd280dfa3f1b4962bd
Sean O'Hara <sohara@sohara.com>
2010-10-17 13:11:29 -0400

    Creation of projects feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ticketee::Application.routes.draw do
+  root :to => "projects#index"
+  resources :projects
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +57,5 @@ Ticketee::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
 end
